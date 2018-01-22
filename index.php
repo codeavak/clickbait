455d6071e51b1bdc460c3ec06769c78a29dfc5a0
Johnny Avakian <javak@yahoo.com>
2018-01-22 15:37:37 -0800

    second_commit

--- index.php
+++ index.php
@@ -8,73 +8,55 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <?php
-        define("TITLE", "Clickbait Headline Neutralizer");
+      define("TITLE","Clickbait Headline Neutralizer");
+    
+    //PHP CONSOLE LOG FUNCTION
+     function logConsole($name, $data = NULL, $jsEval = FALSE)
+ {
+      if (! $name) return false;
 
-        // PHP CONSOLE LOG FUNCTION
+      $isevaled = false;
+      $type = ($data || gettype($data)) ? 'Type: ' . gettype($data) : '';
 
-        function logConsole($name, $data = NULL, $jsEval = FALSE)
-            {
-            if (!$name) return false;
-            $isevaled = false;
-            $type = ($data || gettype($data)) ? 'Type: ' . gettype($data) : '';
-            if ($jsEval && (is_array($data) || is_object($data)))
-                {
-                $data = 'eval(' . preg_replace('#[\s\r\n\t\0\x0B]+#', '', json_encode($data)) . ')';
-                $isevaled = true;
-                }
-              else
-                {
-                $data = json_encode($data);
-                }
+      if ($jsEval && (is_array($data) || is_object($data)))
+      {
+           $data = 'eval(' . preg_replace('#[\s\r\n\t\0\x0B]+#', '', json_encode($data)) . ')';
+           $isevaled = true;
+      }
+      else
+      {
+           $data = json_encode($data);
+      }
 
-            // sanitalize
+      # sanitalize
+      $data = $data ? $data : '';
+      $search_array = array("#'#", '#""#', "#''#", "#\n#", "#\r\n#");
+      $replace_array = array('"', '', '', '\\n', '\\n');
+      $data = preg_replace($search_array,  $replace_array, $data);
+      $data = ltrim(rtrim($data, '"'), '"');
+      $data = $isevaled ? $data : ($data[0] === "'") ? $data : "'" . $data . "'";
 
-            $data = $data ? $data : '';
-            $search_array = array(
-                "#'#",
-                '#""#',
-                "#''#",
-                "#\n#",
-                "#\r\n#"
-            );
-            $replace_array = array(
-                '"',
-                '',
-                '',
-                '\\n',
-                '\\n'
-            );
-            $data = preg_replace($search_array, $replace_array, $data);
-            $data = ltrim(rtrim($data, '"') , '"');
-            $data = $isevaled ? $data : ($data[0] === "'") ? $data : "'" . $data . "'";
-            $js = <<<JSCODE
-        \n<script>
+$js = <<<JSCODE
+\n<script>
+ // fallback - to deal with IE (or browsers that don't have console)
+ if (! window.console) console = {};
+ console.log = console.log || function(name, data){};
+ // end of fallback
 
-         // fallback - to deal with IE (or browsers that don't have console)
+ console.log('$name');
+ console.log('------------------------------------------');
+ console.log('$type');
+ console.log($data);
+ console.log('\\n');
+</script>
+JSCODE;
 
-         if (! window.console) console = {};
-         console.log = console.log || function(name, data){};
-
-         // end of fallback
-
-
-         console.log('$name');
-         console.log('------------------------------------------');
-         console.log('$type');
-         console.log($data);
-         console.log('\\n');
-        </script>
-        JSCODE;
-            echo $js;
-            } //end php console log function
-
-    ?>
+      echo $js;
+ } //end php console log function
+?>
 
         <title>
-            <?php
-                echo TITLE 
-            ?> 
-        </title>
+            <?php echo TITLE ?> </title>
 
         <!-- Bootstrap core CSS -->
         <link href="bs/bootstrap.min.css" rel="stylesheet">
@@ -117,8 +99,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <?php
-echo TITLE ?>
+                <?php echo TITLE ?>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -143,10 +124,9 @@ echo TITLE ?>
             </div>
         </div>
     </nav>
-    <br />
+    <br>
     <h1 class="text-center">
-        <?php
-echo TITLE ?>
+        <?php echo TITLE ?>
     </h1>
     <!-- Page Content -->
     <div class="container">
@@ -163,29 +143,20 @@ echo TITLE ?>
 
         <div class="row">
             <div class="col-sm-6 before-phrase">
-                <?php
-
-                    if (isset($_POST["fix_submit"]))
-                        {
-                        echo "<h1>Clickbait headline:</h1><br /><br />";
-                        echo $_POST["clickbait_headline"];
-                        } 
-                ?>
+                <?php if(isset ($_POST["fix_submit"])) {
+    echo "<h1>Clickbait headline:</h1><br><br>";
+        
+    echo $_POST["clickbait_headline"];} ?>
             </div>
             <div class="col-sm-6 after-phrase">
-                <?php
-
-                    if (isset($_POST["fix_submit"]))
-                        {
-                        echo "<h1>Honest headline:</h1><br /><br />";
-                        convert_to_honest();
-	                   } 
-                ?>
+                <?php if(isset ($_POST["fix_submit"])) {
+    echo "<h1>Honest headline:</h1><br><br>";
+    convert_to_honest();} ?>
             </div>
 
         </div>
-        <br />
-        <br />
+        <br>
+        <br>
         <div class="row">
             <div class="col-sm-12 how-to-use">
                 <h3>How to use?</h3>
@@ -208,44 +179,43 @@ echo TITLE ?>
         </div>
 
         <?php
-
-            function convert_to_honest()
-                {
-                logConsole("Is the form submitted?", "Yes", true);
-                $honestHeadline = strtolower($_POST["clickbait_headline"]);
-                $a = array(
-                    "scientists",
-                    "doctors",
-                    "hate",
-                    "stupid",
-                    "weird",
-                    "simple",
-                    "trick",
-                    "shocked me",
-                    "you'll never believe",
-                    "hack",
-                    "epic",
-                    "unbelievable"
-                );
-                $b = array(
-                    "so-called scientists",
-                    "self-proclaimed doctors",
-                    "are not threatened by",
-                    "average",
-                    "completely normal",
-                    "ineffective",
-                    "method",
-                    "did not shock me at all",
-                    "you'll not be surprised",
-                    "slightly improve",
-                    "boring",
-                    "normal"
-                );
-                $honestHeadline = str_replace($a, $b, $honestHeadline);
-                $honestHeadline = ucfirst($honestHeadline);
-                echo $honestHeadline;
-	           } 
-        ?>
+    function convert_to_honest(){
+            logConsole("Is the form submitted?","Yes",true); 
+          $honestHeadline=strtolower($_POST["clickbait_headline"]);
+    $a= array(
+        "scientists",
+        "doctors",
+        "hate",
+        "stupid",
+        "weird",
+        "simple", 
+        "trick",
+        "shocked me",
+        "you'll never believe",
+        "hack",
+        "epic",
+        "unbelievable"
+    );
+     $b=array(
+        "so-called scientists",
+         "self-proclaimed doctors",
+         "are not threatened by",
+         "average",
+         "completely normal",
+         "ineffective",
+         "method",
+         "did not shock me at all",
+         "you'll not be surprised",
+         "slightly improve",
+         "boring",
+         "normal"
+     );
+     $honestHeadline=str_replace($a,$b,$honestHeadline);
+     $honestHeadline=ucfirst($honestHeadline);
+  echo $honestHeadline;
+        
+       
+    } ?>
             <!-- Bootstrap core JavaScript -->
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -253,5 +223,3 @@ echo TITLE ?>
 </body>
 
 </html>
-
-
